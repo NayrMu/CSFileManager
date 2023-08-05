0b1d4c7182db4421cf58ac41ac04b5045f27aa06
NayrMu <123273762+NayrMu@users.noreply.github.com>
2023-08-05 00:11:32 -0500

    Initialized Project
    Has basic working change directory commands and a rudimentary parser for mods.

---
+++ CSFileManager/ChangeDirectory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SharedDataSpace;
+public class ChangeDirectory {
+	
+	SharedData sharedData;
+	public ChangeDirectory(SharedData sharedDataInstance) {
+		sharedData = sharedDataInstance;
+	}
+
+	public void noMod(string destinationDir) {
+		sharedData.WorkingDir = destinationDir;
+	}
+
+	public void upDir() {
+		string parentDir = sharedData.WorkingDirInfo.Parent.FullName;
+		sharedData.WorkingDir = parentDir;
+	}
+}
---
+++ CSFileManager/MainPrompt.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Reflection;
+using SharedDataSpace;
+
+/*
+ * TODO
+ * 1. Determine if user gave a mod or not
+ * 2. Allow for multiple mods
+ * 3. Flesh out commands and mods
+ */
+
+public class MainPrompt {
+    
+    SharedData sharedData;
+    List<string> commandArr;
+    Type? workingCommand;
+    Type[] allTypes;
+    object? workingCommandInstance;
+    MethodInfo? noMod;
+
+    Dictionary<string, Type> allCommands = new Dictionary<string, Type> {
+        {"cd", typeof(ChangeDirectory) }
+    };
+
+    public MainPrompt() {
+        sharedData = new SharedData();
+        allTypes = Assembly.GetExecutingAssembly().GetTypes();
+    }
+
+    static void Main() {
+        MainPrompt prompt = new MainPrompt();
+        prompt.Run();
+        
+    }
+    public void Run() {
+        while ( true ) {
+            Console.Write($"{sharedData.WorkingDir}:: ");
+            try {
+                sharedData.CommandInput = Console.ReadLine();
+                commandArr = CommandSplitter(sharedData.CommandInput);
+                workingCommand = allCommands[commandArr[0]];
+                noMod = workingCommand.GetMethod("noMod");
+                workingCommandInstance = Activator.CreateInstance(workingCommand, sharedData);
+                if ( false ) {
+                    MethodInfo? modifier = workingCommand.GetMethod(commandArr[1]);
+                    if ( modifier != null ) {
+                        modifier.Invoke(workingCommandInstance, null);
+                    }
+                    else {
+                        Console.WriteLine("Modifier not recognized.");
+                    }
+                }
+                else {
+                    noMod.Invoke(workingCommandInstance, new object[] { commandArr[1] });
+                }
+            }
+            catch (ArgumentException ex) {
+                Console.WriteLine(ex.Message);
+                continue;
+            }
+            
+        }
+    }
+
+    List<string> CommandSplitter(string commandStr) {
+
+        List<string> returnArr = new List<string>();
+        returnArr = commandStr.Split(" ").ToList();
+
+        return returnArr;
+    }
+}
